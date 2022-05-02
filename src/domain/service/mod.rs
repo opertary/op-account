f9ab268b2778ed667d13085772cea16f9af4a851
Christopher Morton <sonro@gmx.com>
2022-05-02 14:20:59 +0100

    Create initial domain services

---
+++ src/domain/service/mod.rs
@@ -0,0 +1,20 @@
+pub mod add_email_to_user;
+pub mod create_user;
+pub mod delete_user;
+pub mod remove_email_from_user;
+pub mod update_user;
+
+pub mod add_user_to_organization;
+pub mod create_organization;
+pub mod delete_organization;
+pub mod list_users_in_organization;
+pub mod remove_user_from_organization;
+pub mod update_organization;
+
+pub mod add_user_to_team;
+pub mod create_organization_team;
+pub mod create_team;
+pub mod delete_team;
+pub mod list_users_in_team;
+pub mod remove_user_from_team;
+pub mod update_team;
